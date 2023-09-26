9a4a7644a804f0b0afb599c9fe742165664b21eb
CleberAGallo <cleber.gallo@etec.sp.gov.br>
2023-09-26 13:34:41 -0300

    26-09

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-const sidebar = document.getElementById('sidebar');
-const toggleButton = document.getElementById('toggleSidebar');
+// Função para controlar a visibilidade do sidebar e a posição do toggle
+function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    const toggleButton = document.getElementById('toggleSidebar');
 
-toggleButton.addEventListener('click', () => {
     sidebar.classList.toggle('hidden');
-    toggleButton.classList.toggle('active');
-});
+
+    // Verifica se o sidebar está visível ou recolhido
+    if (sidebar.classList.contains('hidden')) {
+        // Se estiver recolhido, mova o toggle para o canto esquerdo
+        toggleButton.style.left = '10px';
+    } else {
+        // Se estiver visível, traga o toggle de volta
+        toggleButton.style.left = '250px'; // Ou a largura do seu sidebar
+    }
+}
+
+// Event listener para o botão do sidebar
+const toggleButton = document.getElementById('toggleSidebar');
+toggleButton.addEventListener('click', toggleSidebar);
